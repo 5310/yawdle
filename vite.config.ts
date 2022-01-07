24d55fabf9d6af0df1e652a57cea52f2465b3501
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2022-01-07 19:42:27 +0530

    Fix broken externalizing of Lit

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    rollupOptions: {
-      external: /^lit/,
-    },
     assetsInlineLimit: 0,
   },
 });
