f2b771b4a4805e9bb662b68af403b49f13171e1e
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2022-01-14 13:12:56 +0530

    Adds vite-plugin-pwa for low-fuss offline support

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
+import { registerSW } from 'virtual:pwa-register'
 import '/src/game.ts'
 
-// TODO: Get service workers to work with Vite
-// if ('serviceWorker' in navigator) {
-//   window.addEventListener('load', () => {
-//     navigator.serviceWorker
-//       .register('sw.js')
-//       .then((registration) => {
-//         console.log('Service Worker is registered', registration)
-//       })
-//       .catch((err) => {
-//         console.error('Registration failed:', err)
-//       })
-//   })
-// }
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {},
+})
 
 self.yawdle = document.querySelector('yawdle-game')
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  plugins: [
+    VitePWA({
+      includeAssets: ['robots.txt', 'favicon.svg'],
+      manifest: {
+        name: 'Yawdle',
+        short_name: 'Yet Another Wordle DupLicatE',
+        description: 'Yet Another Wordle DupLicatE',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon@192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon@512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'favicon@512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     assetsInlineLimit: 0,
   },
